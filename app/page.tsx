8cee94c8dd617b55bb9f91aa3ca210b680996351
cserratodev <cserrato.dev@gmail.com>
2026-06-29 22:07:13 -0600

    first commit ELO

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="">
+        <section id="home"></section>
+        <section id="navbar"></section>
+      </main>
+    </div>
+  );
+}
